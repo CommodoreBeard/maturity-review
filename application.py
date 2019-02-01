d39c5bd08db3bf61a02290ef8d0bf1abb7864952
joseph.hughes <joseph.hughes@gfk.com>
2019-02-01 13:21:31 +0000

    app -> application

--- application.py
+++ application.py
@@ -9,26 +9,26 @@ from csv_parser import parse, remove_nulls, gen_weighted_score, \
     gen_max_weighted_score, gen_total_max_weighted_score_per_topic, gen_maturity, gen_avg_maturity
 from write_csv import write
 
-app = Flask(__name__)
-app.config.update(
+application = Flask(__name__)
+application.config.update(
     DEBUG = True,
     SECRET_KEY = 'secret_xxx'
 )
 
-Bootstrap(app)
+Bootstrap(application)
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     form = CsvForm()
     if form.validate_on_submit():
         f = form.csv.data
         filename = "raw_input.csv"
         f.save(os.path.join(
-            app.instance_path, 'uploads', filename
+            application.instance_path, 'uploads', filename
         ))
 
         parsed = parse(os.path.join(
-            app.instance_path, 'uploads', filename
+            application.instance_path, 'uploads', filename
         ))
         parsed = remove_nulls(parsed)
         parsed = gen_weighted_score(parsed)
@@ -37,13 +37,13 @@ def index():
         parsed = gen_maturity(parsed)
         parsed = gen_avg_maturity(parsed)
         write(parsed, os.path.join(
-            app.instance_path, 'processed', "processed.csv"))
+            application.instance_path, 'processed', "processed.csv"))
 
         return send_from_directory(directory=os.path.join(
-            app.instance_path, 'processed'), filename="processed.csv")
+            application.instance_path, 'processed'), filename="processed.csv")
 
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
